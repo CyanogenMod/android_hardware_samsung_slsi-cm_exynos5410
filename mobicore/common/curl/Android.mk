73e4c63c0357f5f8dbdb34e2130d48286498136f
Ethan Chen <intervigil@gmail.com>
2014-04-07 13:11:01 -0700

    exynos5410: Use external/curl for libcurl
    Change-Id: Icb10e4a2b4ead2a63ecf0aa9ab9bba5ce73346dd

--- mobicore/common/curl/Android.mk
+++ mobicore/common/curl/Android.mk
@@ -44,6 +44,8 @@
 # Dan Fandrich
 # November 2011
 
+ifeq ($(USE_MOBICORE_CURL),true)
+
 LOCAL_PATH:= $(call my-dir)
 
 common_CFLAGS := -Wpointer-arith -Wwrite-strings -Wunused -Winline -Wnested-externs -Wmissing-declarations -Wmissing-prototypes -Wno-long-long -Wfloat-equal -Wno-multichar -Wsign-compare -Wno-format-nonliteral -Wendif-labels -Wstrict-prototypes -Wdeclaration-after-statement -Wno-system-headers -DHAVE_CONFIG_H
@@ -88,4 +90,4 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_PRELINK_MODULE := false
 include $(BUILD_SHARED_LIBRARY)
 
-
+endif # USE_MOBICORE_CURL
